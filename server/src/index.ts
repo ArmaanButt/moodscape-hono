b033d4b95427784fef9e9f2a87a2b675617db26a
Armaan Butt <armaan.r.butt@gmail.com>
2025-02-02 01:21:33 -0800

    Embedded and indexed vectors!

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,4 +1,5 @@
 import { Hono } from "hono";
+import OpenAI from "openai";
 
 type Bindings = {
   VECTORIZE: Vectorize;
@@ -16,6 +17,29 @@ app.get("/paintings", async (c) => {
   const { results } = await c.env.DB.prepare("SELECT * FROM paintings;").all();
 
   return c.json(results);
+});
+
+app.get("/query", async (c) => {
+  const openai = new OpenAI({ apiKey: c.env.OPENAI_API_KEY });
+
+  let userQuery = "Feeling excited but nervous about my first day at work.";
+
+  let embeddingResponse = await openai.embeddings.create({
+    input: userQuery,
+    model: "text-embedding-3-small",
+  });
+  console.log(embeddingResponse);
+
+  let matches = await c.env.VECTORIZE.query(
+    embeddingResponse.data[0].embedding,
+    {
+      topK: 1,
+    }
+  );
+
+  //{"matches":{"count":1,"matches":[{"id":"12","score":0.21051788}]}}
+
+  return c.json({ matches });
 });
 
 export default app;
